48f0491e10474901ed36a4ca4322ea96bdc29287
AllenLee <490683824@qq.com>
2016-05-08 13:28:10 +0800

    完成抽象类MyAbstractList

---
+++ src/com/newtalent/www/MyAbstractList.java
@@ -0,0 +1,82 @@
+package com.newtalent.www;
+
+
+/**
+ * 抽象表类
+ * 实现泛型List接口
+ * 
+ * @author AllenLee
+ * @param <E>
+ *
+ */
+public abstract class MyAbstractList<E> implements MyList<E> {
+
+	protected int size = 0;
+	
+	/**
+	 * 默认构造函数
+	 */
+	protected  MyAbstractList() {
+		
+	}
+	
+	/**
+	 * 传入数组以构造线性表
+	 * @param elements
+	 */
+	protected MyAbstractList(E[] elements){
+		for (int i = 0; i < elements.length; i++) {
+			add(elements[i]);
+		}
+	}
+	
+	/**
+	 * 默认在尾部添加新元素
+	 */
+	@Override
+	public void add(E e) {
+		add(size, e);
+	}
+
+
+
+	@Override
+	public boolean contains(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean isEmpty() {
+		return 0 == size;
+	}
+
+
+	@Override
+	public boolean remove(E e) {
+		// TODO Auto-generated method stub
+		if (indexOf(e) >= 0) {
+			remove(indexOf(e));
+			return true;
+		}
+		else {
+			return false;			
+		}
+	}
+
+
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}
+	
+}
---
+++ src/com/newtalent/www/MyList.java
@@ -0,0 +1,102 @@
+package com.newtalent.www;
+
+/**
+ * MyList泛型借口
+ * 定义List的公共操作
+ * 具体的实现可以分为Array & LinkedList
+ * @author AllenLee
+ *
+ */
+public interface MyList<E> {
+	
+	/**
+	 *添加一个元素到List 
+	 * @param e
+	 */
+	public void add(E e);
+	
+	
+	/**
+	 * 按位置添加元素
+	 * @param index
+	 * @param e
+	 */
+	public void add(int index,E e);
+	
+	
+	/**
+	 * 清楚所有元素
+	 */
+	public void clear();
+	
+	
+	/**
+	 * 判断是否含有某元素
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(E e);
+	
+	
+	/**
+	 * 按位置获取元素
+	 * @param index
+	 * @return
+	 */
+	public E get(int index);
+	
+	
+	/**
+	 * 获取某元素的具体索引位置
+	 * @param e
+	 * @return
+	 */
+	public int indexOf(E e);
+	
+	
+	/**
+	 * 判断表是否为空
+	 * @return
+	 */
+	public boolean isEmpty();
+	
+	
+	/**
+	 * 获取最后一个元素的索引位置
+	 * @param e
+	 * @return
+	 */
+	public int lastIndexOf(E e);
+	
+	
+	/**
+	 * 删除某元素
+	 * @param e
+	 * @return
+	 */
+	public boolean remove(E e);
+	
+	
+	/**
+	 * 删除该位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E remove(int index);
+	
+	
+	/**
+	 * 设置某位置的元素
+	 * @param index
+	 * @param e
+	 * @return
+	 */
+	public Object set(int index, E e);
+	
+	/**
+	 * 获取表大小
+	 * @return
+	 */
+	public int size();
+
+}
